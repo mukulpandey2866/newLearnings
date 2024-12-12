a31be3cbd338ec97156fe4a3c53b96dd2cb08d47
mukulpandey2866 <97299715+mukulpandey2866@users.noreply.github.com>
2024-12-12 17:25:26 +0530

    Update simplethings.sh

--- Bash Learnings/simplethings.sh
+++ Bash Learnings/simplethings.sh
@@ -1,3 +1,27 @@
-#!/bin/bash
+!/bin/bash
+Hello, My name is sweatharaji
 
-echo"Started"
+greeting="Hello, "
+name="sweatharaji"
+echo "$greeting My name is $name"
+
+str="Hello"
+length=$((expr length '$str))
+echo "Length of '$str' is $length"
+
+str="Hello"
+length=$(expr length "$str")
+echo "Length of '$str' is $length"
+
+a=10
+b=15
+result=$((a+b))
+echo "sum is $result"
+
+result=$(expr 5 / 3)  # using expr keep space arounf the operator 
+echo "Output: $result"
+
+a=10
+b=20
+result=$(expr $a \< $b)
+echo "$result"
