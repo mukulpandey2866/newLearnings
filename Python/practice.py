093cb5956446556c8228dea959aa290f55d623cc
mukulpandey2866 <97299715+mukulpandey2866@users.noreply.github.com>
2024-12-16 22:07:25 +0530

    Update practice.py

--- Python/practice.py
+++ Python/practice.py
@@ -1,4 +1,8 @@
 print("HEllo","World")
 print("hello"+"World")
 
+# one liner codes if else and others:
+# quizlink:https://play.novelvista.com/quiz-lobby/0179ae72-b1e2-4412-8787-16185608d7d6.co.in
+
+
 print(5+3)
