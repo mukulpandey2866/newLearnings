c020316d1388fcc206d3bb8b2d13841062654ec6
mukulpandey2866 <97299715+mukulpandey2866@users.noreply.github.com>
2024-12-16 22:10:36 +0530

    Update practice.py

--- Python/practice.py
+++ Python/practice.py
@@ -1,6 +1,10 @@
 print("HEllo","World")
 print("hello"+"World")
 
+x=int(input("enter age"))
+y=float(input("enter salary"))
+
+
 # one liner codes if else and others:
 # quizlink:https://play.novelvista.com/quiz-lobby/0179ae72-b1e2-4412-8787-16185608d7d6.co.in
 
