4fc376a2de2635ee85e46cbe695e352bdc37db22
mukulpandey2866 <97299715+mukulpandey2866@users.noreply.github.com>
2025-04-01 15:33:23 +0530

    Update practice.py

--- Python/practice.py
+++ Python/practice.py
@@ -5,7 +5,7 @@ age=int(input("enter age: "))
 salary=float(input("enter salary: "))
 print("Hello, name is Ram, Age is :"+str(age),"Salary is :",salary) 
 # used both here                     +         and          ,
-
+# comment 1
 
 # one liner codes if else and others:
 # quizlink:https://play.novelvista.com/quiz-lobby/0179ae72-b1e2-4412-8787-16185608d7d6.co.in
