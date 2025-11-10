520164f4996373af7797e9a989ce1c2d4bc33c6b
mukulpandey2866 <97299715+mukulpandey2866@users.noreply.github.com>
2025-11-10 09:03:45 +0530

    Fix typo and clean up practice.py
    Corrected a typo in the first line and removed extraneous characters.

--- Python/practice.py
+++ Python/practice.py
@@ -1,4 +1,4 @@
-snsnsjsnhhgjjdjjjgnsneccbfyjejejrjejjwfndjwnwnffjajaertysjjsjrghwydxj
+gusnsnsjsnhhgjjdjjjgnsneccbfyjejejrjejjwfndjwnwnffjajaertysjjsjrghwydxj
 print("HEllo","World")
 print("hello"+"World")
 
