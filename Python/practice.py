16d85b8f202283347412b177193dc03873ad1a8a
mukulpandey2866 <97299715+mukulpandey2866@users.noreply.github.com>
2025-12-11 09:54:25 +0530

    Fix typo in string at the beginning of practice.py

--- Python/practice.py
+++ Python/practice.py
@@ -1,4 +1,4 @@
-esbwnaengkwjwjdjsbjwjaajjahhggusnsnsjsnhhgjjdjjjgnsneccbfyjejejrjejjwfndjwnwnffjajaertysjjsjrghwydxj
+cesbwnaengkwjwjdjsbjwjaajjahhggusnsnsjsnhhgjjdjjjgnsneccbfyjejejrjejjwfndjwnwnffjajaertysjjsjrghwydxj
 print("HEllo","World")
 print("hello"+"World")
 
