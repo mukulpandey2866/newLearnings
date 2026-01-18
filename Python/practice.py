17846929442769b0960128fe19a43d65aa3ac82a
mukulpandey2866 <97299715+mukulpandey2866@users.noreply.github.com>
2026-01-18 10:44:58 +0530

    Update practice.py

--- Python/practice.py
+++ Python/practice.py
@@ -1,4 +1,4 @@
-hshhrgyjhjhfsjdsjjsrdjnhwjhcesbwnaengkwjwjdjsbjwjaajjahhggusnsnsjsnhhgjjdjjjgnsneccbfyjejejgrjejjwfndjwnwnffjajaertysjjsjrghwydxj
+ddhshhrgyjhjhfsjdsjjsrdjnhwjhcesbwnaengkwjwjdjsbjwjaajjahhggusnsnsjsnhhgjjdjjjgnsneccbfyjejejgrjejjwfndjwnwnffjajaertysjjsjrghwydxj
 print("HEllo","World")
 print("hello"+"World")
 
