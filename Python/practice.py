3abb24bd55f23f4215cdf316a118ed069ec139d5
mukulpandey2866 <97299715+mukulpandey2866@users.noreply.github.com>
2026-01-26 14:45:31 +0530

    Fix typo and input prompt in practice.py
    Corrected a typo in the first line and fixed the input prompt.

--- Python/practice.py
+++ Python/practice.py
@@ -1,4 +1,4 @@
-ejussseddhshhrgyjhjhfsjdsjjsrdjnhwjhcesbwnaengkwjwjdjsbjwjaajjahhggusnsnsjsnhhgjjdjjjgnsneccbfyjejejgrjejjwfndjwnwnffjajaertysjjsjrghwydxj
+ffejussseddhshhrgyjhjhfsjdsjjsrdjnhwjhcesbwnaengkwjwjdjsbjwjaajjahhggusnsnsjsnhhgjjdjjjgnsneccbfyjejejgrjejjwfndjwnwnffjajaertysjjsjrghwydxj
 print("HEllo","World")
 print("hello"+"World")
 
