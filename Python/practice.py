27daec7510e4dbeb2c56f18fa159ad228c5aca3a
mukulpandey2866 <97299715+mukulpandey2866@users.noreply.github.com>
2026-07-31 23:52:32 +0530

    Update practice.py

--- Python/practice.py
+++ Python/practice.py
@@ -1,4 +1,4 @@
-hhcchbudeuhjrcnhjyheghjsddfजोdgfjsfghhntfeggdhgvdjhbyhdhhdddjhshhjtfdhejgddewhsrhwjhgnwsghgfuhjhjdjprint("HEllo","World")#hgdsdf
+nshhcchbudeuhjrcnhjyheghjsddfजोdgfjsfghhntfeggdhgvdjhbyhdhhdddjhshhjtfdhejgddewhsrhwjhgnwsghgfuhjhjdjprint("HEllo","World")#hgdsdf
 print("hello"+"World")g
 
 age=int(input("enter age: "))
