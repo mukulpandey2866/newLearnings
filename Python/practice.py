257dc8318bc689c290354eec62caf10428c32e3f
mukulpandey2866 <97299715+mukulpandey2866@users.noreply.github.com>
2026-08-13 09:49:53 +0530

    Fix typo in practice.py

--- Python/practice.py
+++ Python/practice.py
@@ -1,4 +1,4 @@
-ddn hnshhcchbudeuhjrcnhjyheghjsddfजोdgfjsfghhntfeggdhgvdjhbyhdhhdddjhshhjtfdhejgddewhsrhwjhgnwsghgfuhjhjdjprint("HEllo","World")#hgdsdf
+tddn hnshhcchbudeuhjrcnhjyheghjsddfजोdgfjsfghhntfeggdhgvdjhbyhdhhdddjhshhjtfdhejgddewhsrhwjhgnwsghgfuhjhjdjprint("HEllo","World")#hgdsdf
 print("hello"+"World")g
 
 age=int(input("enter age: "))
